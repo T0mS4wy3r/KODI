90420d5b3d91062ed19bc447fa970c5bf81abcd5
emadmahdi <emadmahdi@yahoo.com>
2021-02-20 15:11:45 -0500

    update empty AV to v9.0.0.3

--- ADDONS19/plugin.video.arabicvideos/arabicvideos/__init__.py
+++ ADDONS19/plugin.video.arabicvideos/arabicvideos/__init__.py
@@ -6,7 +6,7 @@ import xbmcgui
 xbmcgui.Dialog().ok('رسالة من المبرمج','برنامج عماد للفيديوهات العربية لا يعمل مع كودي 19 . قم بمسح كودي 19 بالكامل ثم اعد تثبيت كودي والبرنامج والجلد باستخدام هذا الرابط \nhttp://tiny.cc/kodiemad')
 
 
-xbmcgui.Dialog().ok('Message from Emad','Emad "Arabic Videos" is not compatible with Kodi 19 . Delete all kodi 19 and install "KodiEmad" app from this  url (it contains kodi 18.9, skin, and arabic videos program):\nhttp://tiny.cc/kodiemad')
+xbmcgui.Dialog().ok('Message from Emad','Emad "Arabic Videos" is not compatible with Kodi 19 . Delete all kodi 19 and install "KodiEmad" app from this  url (it contains kodi 18.9, skin, and arabic videos program):\n\nhttp://tiny.cc/kodiemad')
 
 
 
